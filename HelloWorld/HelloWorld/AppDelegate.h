fb5517e773e429439b75251d3a45b8f3c9b2cebb
SantoshPawar <sanpaw.cs@gmail.com>
2015-04-07 14:09:15 -0500

    Adding files
    Adding Xcode project files to the repository

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Pawar, Santosh-CW on 4/7/15.
+//  Copyright (c) 2015 Pawar, Santosh-CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Pawar, Santosh-CW on 4/7/15.
+//  Copyright (c) 2015 Pawar, Santosh-CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
